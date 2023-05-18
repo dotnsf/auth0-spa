8735c94f241bc07dcce6dc8bba4de40f36bf259f
K.Kimura <dotnsf@gmail.com>
2023-05-18 10:09:46 +0900

    location.pathname added as redirectUri

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri={window.location.origin}>
+    redirectUri={window.location.origin+window.location.pathname}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
